c371c1f20cde5fc2c55ab6ed9c00a345b67e5f1b
Nick Christ <nchrist01@gmail.com>
2015-11-15 13:04:09 +0000

    Added code for display_with_guesses and check_win_or_lose

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,12 +10,13 @@ class HangpersonGame
   attr_accessor :word
   attr_accessor :guesses
   attr_accessor :wrong_guesses
-  
+  attr_reader :max_game_guesses
   
   def initialize(word)
-    @word = word
+    @word = word.downcase
     @guesses = ''
     @wrong_guesses = ''
+    @max_game_guesses = 7
   end
   
   def guess (letter)
@@ -23,23 +24,37 @@ class HangpersonGame
     if letter == nil or letter.size == 0 or letter.size > 1 or letter =~ /\W/
       raise ArgumentError.new('Guess cannot be blank, empty, a non alphabetic character, or more than one character')  
     end
-    
-    if self.guesses.include? letter or self.wrong_guesses.include? letter then false
+    lower_case_letter = letter.downcase
+    if self.guesses.include? lower_case_letter or self.wrong_guesses.include? lower_case_letter then false
       #Already guessed this letter
     else
-      if self.word.include? letter then self.guesses = self.guesses + letter
-      else self.wrong_guesses = self.wrong_guesses + letter
+      if self.word.include? lower_case_letter then self.guesses = self.guesses + lower_case_letter
+      else self.wrong_guesses = self.wrong_guesses + lower_case_letter
       end
     end
-     
-    
   end
-
+  
+  def check_win_or_lose
+    case
+    when self.guesses.length == self.word.length then :win
+    when self.wrong_guesses.length == self.max_game_guesses then :lose
+    else :play
+    end
+  end     
+  
+  def word_with_guesses
+    if self.guesses == "" then 
+      self.word.gsub(/./,"-")
+    else
+      self.word.gsub(/[^#{self.guesses}]/,"-")
+    end
+  end
+  
   def self.get_random_word
+   self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
 end
